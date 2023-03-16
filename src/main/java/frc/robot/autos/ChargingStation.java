5b1f7eaa3fcd60c6f2a5db30456b6034df79f3e2
ftc519 <ftc519@gmail.com>
2023-03-15 21:37:14 -0400

    Slow mode now works

--- src/main/java/frc/robot/autos/ChargingStation.java
+++ src/main/java/frc/robot/autos/ChargingStation.java
@@ -1,13 +1,18 @@
 package frc.robot.autos;
 
+import com.ctre.phoenix.sensors.Pigeon2;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Swerve;
 
 public class ChargingStation extends CommandBase{
     private Swerve swerve;
+    private Pigeon2 gyro;
     private int pidSlot = 1;
 
-    public ChargingStation(Swerve swerveSubsystem){
+    public ChargingStation(Swerve swerveSubsystem, Pigeon2 gyro){
+        this.gyro = gyro;
+
         swerve = swerveSubsystem;
         swerve.setPID(10, 0, 0, pidSlot);
         swerve.setMinMax(-0.2, 0.2, pidSlot);
